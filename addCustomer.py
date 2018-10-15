98cdfc18696f69ee117cae76a989952fe0438ced
CStrnad <chrisstrnad@gmail.com>
2018-10-14 23:00:05 -0500

    Added commenting to increase readability of script.

--- addCustomer.py
+++ addCustomer.py
@@ -6,13 +6,18 @@
 
 print("         A1 Party Suppliers Order Entry System")
 
-str_cust_name = str(input("Enter Customer Name: "))
+str_cust_name = str(input("Enter Customer Name: "))   #Request the Customers name.
+str_address = str(input('Enter Address:'))   #Request the address of the customer.
+str_address_two = str(input('Enter City, State, zip: '))   #Request city, state, and zip from Customer.
+
+# Repeat information back to User.
+print('\n')
 print('Customer Name:', str_cust_name)
-str_address = str(input('Enter Address:'))
 print('Address: ', str_address)
-str_address_two = str(input('Enter City, State, zip: '))
 print('City, State, zip: ', str_address_two)
-affirmation_check = str(input('Click "Y" to add customer: '))
+
+print('\n')
+affirmation_check = str(input('Enter "Y" to add customer: '))   #Request the user to confirm the data entered is correct.
 
 if str(affirmation_check) == 'y' or 'Y' or 'yes' or 'Yes':
     print('Added to registry.')
