71d724c8f842a32d323e9c3dbe6ed40aa593bfd7
CStrnad <chrisstrnad@gmail.com>
2018-10-14 22:05:34 -0500

    Fixed formatting

--- displayCustomers.py
+++ displayCustomers.py
@@ -4,7 +4,7 @@
 #          to display a list
 #          of customer data.
 
-print("              A1 Party Suppliers Order Entry System")
+print("           A1 Party Suppliers Order Entry System")
 print("                   List of Customers")
 print("---------------------------------------------------------------------")
 print("Name                 Address                        City, State, Zip")
