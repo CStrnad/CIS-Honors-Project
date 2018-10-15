2f66bb76fe1d32ece2567c760e187a04c082f3be
CStrnad <chrisstrnad@gmail.com>
2018-10-14 21:54:23 -0500

    Created and set up Header to be displayed.

---
+++ displayCustomers.py
@@ -0,0 +1,12 @@
+# File_Name: displayCustomers.py
+# Author: Christopher Strnad
+# Purpose: Create a program
+#          to display a list
+#          of customer data.
+
+print("              A1 Party Suppliers Order Entry System")
+print("                   List of Customers")
+print("---------------------------------------------------------------------")
+print("Name                 Address                        City, State, Zip")
+print("---------------------------------------------------------------------")
+
