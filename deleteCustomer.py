c1f9559cb45e7025bf9167abe224360f08369f51
CStrnad <chrisstrnad@gmail.com>
2018-10-14 23:14:07 -0500

    Created file. Code not implemented yet.

---
+++ deleteCustomer.py
@@ -0,0 +1,8 @@
+# File_Name: deleteCustomer.py
+# Author: Christopher Strnad
+# Purpose: Create a program that
+#          enables the user to
+#          delete customers.
+
+#Not implemented yet.
+
